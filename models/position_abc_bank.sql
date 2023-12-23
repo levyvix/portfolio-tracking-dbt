91715fb2c2a08a2b41a5ed220ef9ae06a1adcbcf
Levy Nunes <levy.vix@protonmail.com>
2023-12-23 21:17:07 +0000

    change schema

--- models/position_abc_bank.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-*,
-position_value - cost_base as unrealized_profit,
-round(
-    (position_value - cost_base) / cost_base
-    , 5) as unrealized_profit_pct
-from {{source("abc_bank", 'abc_bank_position')}}
